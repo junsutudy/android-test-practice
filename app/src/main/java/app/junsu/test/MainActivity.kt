bf92345eceeb84b804ce9e201302f14601b235c2
junsuPark <krpjs0508@gmail.com>
2024-01-08 21:41:53 +0900

    test

--- app/src/main/java/app/junsu/test/MainActivity.kt
+++ app/src/main/java/app/junsu/test/MainActivity.kt
@@ -3,26 +3,31 @@ package app.junsu.test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import app.junsu.test.ui.theme.TestExampleTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TestExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                BottomSheetScaffold(
+                    sheetContent = {},
+                ) { paddingValues ->
+                    Column(
+                        modifier = Modifier.padding(paddingValues),
+                    ) {
+                        Alignment.BottomEnd
+                    }
                 }
             }
         }
--- app/src/test/java/app/junsu/test/ExampleUnitTest.kt
+++ app/src/test/java/app/junsu/test/ExampleUnitTest.kt
@@ -2,16 +2,34 @@ package app.junsu.test
 
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `Test addition of String and Double`() {
+        assert(AdditionManager.calculateToDouble("24", 2) == 26.0)
+    }
+
+    @Test
+    fun `Test addition of Double and Int`() {
+        assert(AdditionManager.calculateToDouble(2.0, 6) == 8.0)
+    }
+}
+
+object AdditionManager {
+    fun calculateToDouble(a: Any, b: Any): Double {
+
+        val aInNumber = try {
+            a.toString().toDouble()
+        } catch (_: Exception) {
+            throw NumberFormatException()
+        }
+
+        val bInNumber = try {
+            b.toString().toDouble()
+        } catch (_: Exception) {
+            throw NumberFormatException()
+        }
+
+        return aInNumber + bInNumber
     }
 }
