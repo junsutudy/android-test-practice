e54905101631869e2dd2968cb433eb816228fb23
Junsu Park <krpjs0508@gmail.com>
2024-02-01 15:10:38 +0900

    code

--- app/src/main/java/app/junsu/test/MainActivity.kt
+++ app/src/main/java/app/junsu/test/MainActivity.kt
@@ -3,7 +3,12 @@ package app.junsu.test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import app.junsu.test.ui.music.MusicScreen
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import app.junsu.test.ui.home.HomeScreen
+import app.junsu.test.ui.playlist.PlayListScreen
 import app.junsu.test.ui.theme.TestExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +16,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TestExampleTheme {
-                MusicScreen()
+                MusicApp()
             }
+        }
+    }
+}
+
+@Composable
+fun MusicApp() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home",
+    ) {
+        composable("home") {
+            HomeScreen(
+                openPlayList = {
+                    navController.navigate("play_list")
+                },
+            )
+        }
+        composable("play_list") {
+            PlayListScreen(
+                navigateUp = navController::navigateUp,
+            )
         }
     }
 }
